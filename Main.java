94e19a558b2a1e019d04149f0af30b56d89016d9
Peter Olson <peter.olson@bfacademy.de>
2024-12-13 06:59:18 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,36 +1,8 @@
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
     System.out.println("Hello world!");
   }
 
-  /*Exercise 1 */
-  public static boolean isEven (int x)
-  {
-    /* missing code */
-  }
-
-
-  /* Exercise 2 */
-  public static String triangleType(int a, int b, int c)
-  {
-	   
-       /* missing code */
-  
-      
-  }
-
-
-  /* Exercise 3 */
-  public static String classifyNumber(int num)
-  {
-     /* missing code */
-  }
-
-
-  /* Exercise 4 */
-  public static boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2)
-  {
-    /* missing code */
-  }
 }
